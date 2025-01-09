6a6460f9762fe6946672c1accc14e2b042a7f716
Mark <madlyinanger37@gmail.com>
2025-01-09 07:49:50 -0600

    Adding and fleshing out the log component

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameContent = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    playerNameContent = <input type="text" required value={playerName} onChange={handleChange} />;
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {playerNameContent}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? 'Edit' : 'Save'}</button>
+    </li>
+  );
+}
